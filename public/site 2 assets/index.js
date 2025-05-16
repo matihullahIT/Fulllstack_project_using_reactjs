1751d75b2e19ad2e3b329457c923589931911c2d
matihullahIT <matihurrehman.official@gmail.com>
2025-05-16 23:09:16 +0500

    Image Assets

---
+++ public/site 2 assets/index.js
@@ -0,0 +1,52 @@
+export const navbar=[
+    {
+        id:1,
+        text:"Madelyn Torff",
+    },
+    {
+        id:2,
+        text:"About",
+    },
+    {
+        id:3,
+        text:"Projects",
+    },
+    {
+        id:4,
+        text:"Contacts",
+    },
+]
+export const banner ={
+    img:"",
+    text:"UI/UX Designer",
+    heading:"Hello,  my name is Madelyn Torff",
+    details:"Short text with details about you, what you do or your professional career. You can add more information on the about page.",
+    btn:["Projects","LinkedIn"]
+}
+const projectname ="Project Name"
+const btn ="View Project"
+export const Cards=[
+    {
+        id:1,
+        name:projectname,
+        details:"I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.",
+        btn:btn
+    },
+    {
+        id:2,
+        name:projectname,
+        details:"What was your role, your deliverables, if the project was personal, freelancing.",
+        btn:btn,
+    },
+    {
+        id:3,
+        name:projectname,
+        details:"You can also add in this description the type of the project, if it was for web, mobile, electron.",
+        btn:btn
+    },
+] 
+ export const foother=
+    {
+        id:1,
+        icon:["/footer.svg","/footer 2svg.svg","/footer3.svg"]
+    }
