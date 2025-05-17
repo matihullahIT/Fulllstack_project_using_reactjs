1b0bf64709a15752365baa4eec9ed38a16b4b327
matihullahIT <matihurrehman.official@gmail.com>
2025-05-17 23:25:49 +0500

    bannerimage setting

---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = () => {
+  return (
+    <div className="flex items-center justify-center h-screen border-2">
+        <div className="border-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut perspiciatis ab veniam quos, voluptatibus delectus magnam eligendi consequuntur, alias iusto sunt eum. Voluptatum ea ut sed at voluptas officia, sapiente fugit fuga delectus dolorum repellat dolore distinctio consequuntur animi odit sunt eum ipsam? Nemo fuga nihil, quos libero suscipit harum ipsa maiores voluptatum aut sapiente accusantium tempora magnam asperiores autem consectetur molestiae tempore dolore maxime incidunt rem molestias? Aut mollitia iste, sequi qui veritatis, quasi odit sunt, officiis inventore eos facere rerum sint blanditiis ea pariatur? Consequuntur minima facere, fuga odio ipsum sunt vero quod repellendus dolorum placeat doloremque, nisi omnis, blanditiis nobis quas rerum maxime aliquid ab ducimus voluptatibus reprehenderit aperiam sed ex. Repellendus magnam incidunt ducimus corrupti illo, voluptatem accusamus culpa ipsam quisquam neque nostrum quae ratione aliquid adipisci deserunt accusantium fuga excepturi? Iste tenetur atque temporibus cum. Porro expedita sint consequuntur fugit soluta dolores voluptatem error explicabo odio! Ratione recusandae nobis vitae repudiandae quia maxime praesentium temporibus soluta velit a sunt cumque cupiditate perferendis nisi nulla amet modi, corrupti quo repellendus quibusdam deleniti distinctio? Molestiae tenetur ex enim impedit. Ipsum magni quod deserunt error maiores excepturi aliquam sequi culpa quae rerum illo, ullam dolore ducimus est adipisci aut alias eum laboriosam nam ex harum ratione molestiae pariatur. Laboriosam harum nam facere dolore veritatis quod fugit eaque placeat sit pariatur est mollitia asperiores libero aperiam saepe ratione voluptas commodi, omnis provident recusandae fugiat reprehenderit reiciendis exercitationem explicabo? Quo voluptas voluptatum fuga repellat iusto officia consectetur ipsa ipsum libero incidunt, harum debitis quibusdam quas dolorum doloremque. Perferendis sit, omnis fuga recusandae saepe ipsum nemo? Corporis, quam a itaque delectus vitae rem voluptates eos! Odio id delectus impedit eius voluptatum rerum sint, officiis reprehenderit fugiat. Cumque ullam illo porro eveniet doloremque dolorem aperiam, perferendis nostrum ut aspernatur ipsa voluptate? Ipsa!
+        </div>      
+        <div> 
+        <img src="./assets/bannerimg.svg"  className="h-screen w-auto" alt="" srcset="" />
+        </div>      
+    </div>
+  )
+}
+
+export default Banner
