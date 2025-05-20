2bf52f5c09c99197e5e0cc07283309b2d2a042e7
matihullahIT <matihurrehman.official@gmail.com>
2025-05-20 20:09:20 +0500

    update src/constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const navbarSection=[
     {
         id:1,
         text:"Madelyn Torff",
-        link:"/home"
+        link:"/"
     },
     {
         id:2,
@@ -22,34 +22,37 @@ export const navbarSection=[
 ]
 export const bannerSection ={
     text:"UI/UX Designer",
-    heading:"Hello,  my name is Madelyn Torff",
+    heading:"Hello, <b/> my name is Madelyn Torff",
     details:"Short text with details about you, what you do or your professional career. You can add more information on the about page.",
     btn:["Projects","LinkedIn"]
 }
 const projectname ="Project Name"
 const btn ="View Project"
-export const Cards=[
+export const testimonialSection=[
     {
         id:1,
         name:projectname,
+        img:"/site 2 assets/testmonial 1.svg",
         details:"I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.",
         btn:btn
     },
     {
         id:2,
         name:projectname,
+        img:"/site 2 assets/testimonial 2.svg",
         details:"What was your role, your deliverables, if the project was personal, freelancing.",
         btn:btn,
     },
     {
         id:3,
         name:projectname,
+        img:"/site 2 assets/testimonails 3.svg",
         details:"You can also add in this description the type of the project, if it was for web, mobile, electron.",
         btn:btn
     },
 ] 
- export const foother=
+ export const footersection=
     {
-        id:1,
-        icon:["/footer.svg","/footer 2svg.svg","/footer3.svg"]
+        
+        icon:["/site 2 assets/footer.svg","/site 2 assets/footer 2svg.svg","/site 2 assets/footer3.svg"]
     }
