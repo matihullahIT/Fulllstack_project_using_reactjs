5cc5c9adbc3cfef3f0b75f05fbe10e8731bc29a6
matihullahIT <matihurrehman.official@gmail.com>
2025-05-23 13:02:17 +0500

    update Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,10 +1,10 @@
-const Popup = ({props}) => {
+const Popup = ({props,button}) => {
   return (
     <div className={`absolute w-screen h-screen bg-gray-400 backdrop-blur-md`}>
         <div className="w-full h-full bg-white">
             {props}
         </div>
-
+        <div>{button}</div>
     </div>
   )
 }
