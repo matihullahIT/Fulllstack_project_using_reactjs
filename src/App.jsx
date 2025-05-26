d3126000e71a8e911537eaff6be4cf4e9843d693
matihullahIT <matihurrehman.official@gmail.com>
2025-05-26 20:16:37 +0500

    refactor: improve code structure and readability in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import "./index.css"
-import Navbar from "./components/Navbar";
-import Banner from "./components/Banner";
-import Testimonial from "./components/Testimonisal";
-import Footer from "./components/Footer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Aboutus from "./components/Aboutus";
 import { useEffect } from "react";
-
+import Login from "./components/Login"
+import Register from "./components/Register"
 function App() {
   useEffect(() => {
     document.documentElement.style.scrollBehavior = "smooth";
@@ -17,25 +13,12 @@ function App() {
 
   return (
     <div className="">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path="/" element={
-            <div className="flex flex-col items-center justify-center">
-              <div>
-                <div className="z-10  items-center justify-center">
-                  <Navbar />
-                </div>
-                <Banner />
-              </div>
-              <div>
-              </div>
-              <Testimonial/>
-              <Footer/>
-            </div>
-          } />
-          <Route path="/about" element={<Aboutus />} />
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </div>
   )
 }
